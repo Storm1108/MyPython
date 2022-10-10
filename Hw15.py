1e3ec90c472b9708e0ac107ab036808de81960d3
Andrey <63869064+Storm1108@users.noreply.github.com>
2022-10-10 21:59:14 +0300

    Delete Hw15.py

--- Hw15.py
+++
@@ -1,5 +0,0 @@
-x1 = float(input("Enter X coordinate of first point: "))
-y1 = float(input("Enter Y coordinate of first point: "))
-x2 = float(input("Enter X coordinate of second point: "))
-y2 = float(input("Enter Y coordinate of second point: "))
-print(f"Distance between ({x1}, {y1}) and ({x2}, {y2}) points is {(((x1-x2)**2)+((y1-y2)**2))**0.5:.3f}")
