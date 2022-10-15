5af5a72ede8406ad0f058ee7f063dc7502aef8ca
Storm1108 <mail.laser@yandex.ru>
2022-10-16 00:49:12 +0300

    HW2 UPD

--- HomeWork/Hw2/Hw25.py
+++ HomeWork/Hw2/Hw25.py
@@ -1,11 +1,12 @@
 import random
 
-num_list_start = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+num_list_start = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 num_list_rand = []
 checklist = []
 while len(num_list_rand) < len(num_list_start):
-    temp_random = round(random.random() * (len(num_list_start))) - 1
+    temp_random = round(random.random() * (len(num_list_start) -1))
     if temp_random not in checklist:
+        print(temp_random)
         num_list_rand.append(num_list_start[temp_random])
         checklist.append(temp_random)
 print(num_list_start)
