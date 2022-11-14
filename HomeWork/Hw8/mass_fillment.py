e0bdbc2194619bee9dab168b05a83a3afd6778c6
Storm1108 <mail.laser@yandex.ru>
2022-11-14 23:39:39 +0300

    hw8dev4.5

--- HomeWork/Hw8/mass_fillment.py
+++ HomeWork/Hw8/mass_fillment.py
@@ -61,9 +61,9 @@ surnames, names, fathnames = zip(*array)
 
 sexes = [''.join(sample(('М', 'Ж'), 1)) for i in range(400)]
 medials = [round(uniform(2, 5), 2) for i in range(400)]
-class_ids = [randint(1, 22) for i in range(100)]
+class_ids = [randint(1, 22) for i in range(400)]
 
-teacher_ids = [randint(1, 20) for i in range(20)]
+teacher_ids = [randint(1, 20) for i in range(150)]
 subjects = [''.join(sample(['Математика', 'Русский язык', 'Физика', 'География', 'Информатика'], 1)) for i in range(100)]
 passport_ser = [randint(1000, 9999) for i in range(20)]
 passport_num = [randint(100000, 999999) for i in range(20)]
@@ -71,6 +71,7 @@ notes = ['-' for i in range(400)]
 dates = data_gen()
 dates_teachers = data_gen_old()
 fathnames = list(map(lambda a: str(a), fathnames))
+
 classes_mas = list(zip(classes_num, classes_lit))
 multi_data_load(input_classes, classes_mas)
 
